50409599d29595d3e0902ad0207f940dafe314fd
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2022-10-08 19:54:11 +0530

    New Method Added

--- src/test/java/commonFunctions/Sample.java
+++ src/test/java/commonFunctions/Sample.java
@@ -1,9 +1,22 @@
 package commonFunctions;
 
 public class Sample {
-
+public void add()
+{
+	int a=456;
+	int b=876;
+	int		c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=456;
+	int b=876;
+	int		c=a/b;
+	System.out.println(c);
+}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
